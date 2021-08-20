3f0336898487e9b1ba76d653e41bd6df3c064823
米滋 <1571883349@qq.com>
2021-08-20 11:15:29 +0800

    zhuce

--- js/app/routes/users.js
+++ js/app/routes/users.js
@@ -8,12 +8,24 @@ router.post('/login', async (req, res, next) => {
   console.log(arr);
   let sqlstr = `SELECT * FROM user WHERE username=? AND password=?`
   let data = await miziquery(sqlstr, arr)
-  // data = JSON.parse(data)
   console.log(data);
-  // if (req.body.username) {
-
-  // }
-  res.send('respond with a resource');
+  if (data.length > 0) {
+    res.send('登录成功');
+  }
+  else {
+    res.send('登录失败,请检查用户名和密码')
+  }
+});
+router.post('/register', async (req, res, next) => {
+  let arr = [req.body.username, req.body.password]
+  let sqlstr = `SELECT * FROM user WHERE username=? AND password=?`
+  let data = await miziquery(sqlstr, arr)
+  if (data.length > 0) {
+    res.send('登录成功');
+  }
+  else {
+    res.send('登录失败,请检查用户名和密码')
+  }
 });
 
 module.exports = router;
