48024a437fdfc12764f1ed424503683fc38b9d35
米滋 <1571883349@qq.com>
2021-08-20 10:53:38 +0800

    完成登录验证

--- js/app/routes/users.js
+++ js/app/routes/users.js
@@ -6,7 +6,7 @@ router.post('/login', async (req, res, next) => {
   console.log(req.body);
   let arr = [req.body.username, req.body.password]
   console.log(arr);
-  let sqlstr = `SELECT * FROM user WHERE username=? AND 'password'=?`
+  let sqlstr = `SELECT * FROM user WHERE username=? AND password=?`
   let data = await miziquery(sqlstr, arr)
   // data = JSON.parse(data)
   console.log(data);
